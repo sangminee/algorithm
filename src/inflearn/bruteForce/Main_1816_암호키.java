7ea5889ffc04e2f5c45842e941afc781558dc058
sangminee <lsmlee99@gmail.com>
2023-10-20 03:00:00 +0900

    feat : <boj> 암호키 (1816)

--- src/inflearn/bruteForce/Main_1816_암호키.java
+++ src/inflearn/bruteForce/Main_1816_암호키.java
@@ -1,4 +1,4 @@
-package inflearn.bruteForce.repeated;
+package inflearn.bruteForce;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,8 +12,8 @@ import java.io.InputStreamReader;
  * - 만약 S의 모든 소인수가 1_000_000 보다 크면 => YES
  * - 아니면 NO
  *
- * [소인수 구하는 방법]
- * - 소인수 : 1을 제외한 약수
+ * [소인수]
+ * - 1을 제외한 약수
  *
  * */
 public class Main_1816_암호키 { // https://www.acmicpc.net/problem/1816
@@ -22,15 +22,18 @@ public class Main_1816_암호키 { // https://www.acmicpc.net/problem/1816
 
     public static void main(String[] args) throws IOException {
         int number_count = Integer.parseInt(br.readLine()); // 수의 개수
-
         while(number_count --> 0){
-            Long s = Long.valueOf(br.readLine()); // 확인하고자 하는 수
+            Long s = Long.valueOf(br.readLine());
             for(int i=2; i<=1_000_000; i++){
                 if(s % i == 0){
+                    // 1_000_000 이하의 약수가 존재함
                     System.out.println("NO");
                     break;
                 }
-                if(i == 1_000_000) System.out.println("YES"); // 100만 이후까지 프로그램이 멈추지 않았음
+
+                if(i == 1_000_000){ // 이 전까지 프로그램이 멈추지 않았음
+                    System.out.println("YES");
+                }
             }
         }
     }
