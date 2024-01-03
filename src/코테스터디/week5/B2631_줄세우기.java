4a5ec13e6ff5f598253f387dd0bb20cff4fad565
sangminee <lsmlee99@gmail.com>
2024-01-03 16:59:14 +0900

    week7

---
+++ src/코테스터디/week5/B2631_줄세우기.java
@@ -0,0 +1,13 @@
+package 코테스터디.week5;
+
+import java.io.*;
+import java.util.*;
+
+public class B2631_줄세우기 { // https://www.acmicpc.net/problem/2631
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 아이들 수
+
+    }
+}
