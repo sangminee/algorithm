d70c2726b7c0bf255a6bdcfd96586a6206824dd0
sangminee <lsmlee99@gmail.com>
2024-01-18 09:43:03 +0900

    24/01/18/목

---
+++ src/코테스터디/week7/B2294_동전2.java
@@ -0,0 +1,42 @@
+package 코테스터디.week7;
+
+import java.io.*;
+import java.util.*;
+
+// n을 사용해 k 만들기
+public class B2294_동전2 { // https://www.acmicpc.net/problem/2294
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        // i개를 가지고 j를 만들 수 있는 최소 경우의 수
+        int answer = Integer.MAX_VALUE;
+
+        int[][] minCoin = new int[n+1][k+1];
+        for(int i=0; i<=n; i++){
+            for(int j=1; j<=k; j++){
+                minCoin[i][j] = 10_002;
+            }
+        }
+
+        for(int i=1; i<=n; i++){
+            int coin = Integer.parseInt(br.readLine());
+            for(int j=1; j<=k; j++){
+                minCoin[i][j] = minCoin[i-1][j];
+
+                if(j>=coin){
+                    minCoin[i][j] = Math.min(minCoin[i][j], minCoin[i][j-coin] + 1);
+                }
+            }
+            // k 의 최솟값
+            if(minCoin[i][k] != 0 ) answer = Math.min(answer, minCoin[i][k]);
+        }
+
+        if(answer == 10_002)
+            answer = -1;
+        System.out.println(answer);
+
+    }
+}
---
+++ src/코테스터디/week7/B4781_사탕가게.java
@@ -0,0 +1,48 @@
+package 코테스터디.week7;
+
+import java.io.*;
+import java.util.*;
+
+public class B4781_사탕가게 { // https://www.acmicpc.net/problem/4781
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+
+        while(true){
+
+            String input = br.readLine();
+            if(input.equals("0 0.00")){
+                break;
+            }
+
+            st = new StringTokenizer(input);
+            int n = Integer.parseInt(st.nextToken()); // 사탕 종류의 수
+            int money = subStr(st.nextToken()); // 상근이가 가지고 있는 돈의 양 (100을 곱해서 int 형으로 만든 다음에 0부터 money까지 탐색)
+
+            // i로 살 수 있는 가장 큰 칼로리를 저장
+            long[] dp = new long[money + 1];
+
+            for(int i=1; i<=n; i++){
+                st = new StringTokenizer(br.readLine());
+                int calorie = Integer.parseInt(st.nextToken()); // 사탕의 칼로리
+                int price = subStr(st.nextToken()); // 가격
+
+                for(int j=price; j<=money; j++){
+                    dp[j] = Math.max(dp[j], dp[j-price] + calorie);
+                }
+            }
+            sb.append(dp[money]+"\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static int subStr(String str){
+        double a = Double.parseDouble(str);
+        return (int) (a * 100 + 0.5);
+    }
+
+}
