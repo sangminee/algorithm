c630b36610c4857f5a810731325a29e0cededce0
sangminee <81500474+sangminee@users.noreply.github.com>
2024-03-11 01:42:56 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,9 +1,9 @@
 -- 코드를 입력하세요
 
 SELECT ANIMAL_TYPE
-      ,case when NAME is NULL then 'No name'
-            else NAME 
-       end as NAME
-      ,SEX_UPON_INTAKE
-from ANIMAL_INS
-order by ANIMAL_ID;
+      , CASE WHEN NAME IS NULL THEN 'No name'
+             ELSE NAME
+        END AS NAME
+      , SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID; 
