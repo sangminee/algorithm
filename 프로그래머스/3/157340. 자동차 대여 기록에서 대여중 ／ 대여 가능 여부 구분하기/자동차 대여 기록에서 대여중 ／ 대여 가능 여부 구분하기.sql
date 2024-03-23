ccbc00b023dc02e7edf7683febe9248f86c46c19
sangminee <81500474+sangminee@users.noreply.github.com>
2024-03-23 15:41:25 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,12 +1,16 @@
 -- 코드를 입력하세요
-SELECT CAR_ID, 
-case when CAR_ID IN (
-    select CAR_ID 
-    from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-    where '2022-10-16' between START_DATE and END_DATE) 
-    then '대여중'
-    else '대여 가능'
-    end as AVAILABILITY
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-GROUP BY CAR_ID
-ORDER BY CAR_ID DESC;
+
+-- 대여 중인 자동차 
+
+
+SELECT a.CAR_ID 
+       ,case AVAILABILITY WHEN '대여중' THEN '대여중'
+            else '대여 가능'
+        end as AVAILABILITY
+FROM (SELECT CAR_ID FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY GROUP BY CAR_ID) a
+LEFT JOIN (SELECT CAR_ID, '대여중' as AVAILABILITY
+           FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+           WHERE to_char(START_DATE, 'YYYYMMDD') <= '20221016'
+           AND to_char(END_DATE, 'YYYYMMDD') >= '20221016') b
+ON a.CAR_ID = b.CAR_ID
+ORDER BY a.car_id desc;
